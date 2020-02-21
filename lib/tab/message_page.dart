c1339afe23ecad7585be69d92a200485529c230a
wuhongxing <357898849@qq.com>
2020-02-21 14:29:55 +0800

    完成tab_navigator

--- lib/tab/message_page.dart
+++ lib/tab/message_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class MessagePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Text('message'),
+    );
   }
 }
